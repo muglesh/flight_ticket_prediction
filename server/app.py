182df7bc43242dd9da75feda69613dab3a7d62f0
muglesh <149783885+muglesh@users.noreply.github.com>
2024-04-18 10:06:47 +0530

    Add files via upload

---
+++ server/app.py
@@ -0,0 +1,57 @@
+from flask import Flask,  request, jsonify
+import util2 as util2
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return "Hi"
+
+
+@app.route('/get_location_names')
+def get_location_names():
+    response = jsonify({
+        "locations": util2.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/get_Class_names')
+def get_class_names():
+    response = jsonify({
+
+        "Class": util2.get_class_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/get_airline_names')
+def get_airline_names():
+    response = jsonify({
+
+        "airlines": util2.get_airline_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/predict_flight_price', methods=['POST'])
+def predict_flight_price():
+    airline = str(request.form['airline'])
+    Class = int(request.form['Class'])
+    destination = str(request.form['destination'])
+    departure = str(request.form['departure'])
+    day = str(request.form['day'])
+    stops = int(request.form['stops'])
+
+    response = jsonify({
+        'estimated_price': util2.get_estimated_price(airline, Class, destination, departure, day, stops)
+    })
+
+    response.headers.add('Access-control-Allow-Origin', '*')
+
+    return response
+
+
